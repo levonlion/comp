5e9a573b109da96aff113a407914a070afcc42af
levonlion <72524427+levonlion@users.noreply.github.com>
2020-10-07 23:24:44 +0300

    Update Header.h

--- OS/Header.h
+++ OS/Header.h
@@ -1,1 +1,13 @@
-Aa
+#pragma once
+
+struct ComplexNum{
+	int a, b;
+
+	ComplexNum(int a, int b);	
+	ComplexNum();
+	int mod();
+
+	ComplexNum operator+(ComplexNum& operand);
+	ComplexNum operator-(ComplexNum& operand);
+	ComplexNum operator*(int constant);
+};
