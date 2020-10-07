90bba4d29c62665d6ff6fa296498762c1e1f7a70
levonlion <72524427+levonlion@users.noreply.github.com>
2020-10-07 23:25:50 +0300

    Update compstr.cpp

--- OS/compstr.cpp
+++ OS/compstr.cpp
@@ -1,1 +1,36 @@
-a
+#include "Header.h"
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+ComplexNum::ComplexNum(int a, int b){
+	this->a = a;
+	this->b = b;
+}
+
+ComplexNum::ComplexNum(){
+	this->a = 0;
+	this->b = 0;
+}
+
+int ComplexNum::mod(){
+	return sqrt(a * a + b * b);
+}
+
+ComplexNum ComplexNum::operator+(ComplexNum& other){
+	this->a += other.a;
+	this->b += other.b;
+	return *this;
+}
+
+ComplexNum ComplexNum::operator-(ComplexNum& other){
+	this->a -= other.a;
+	this->b -= other.b;
+	return *this;
+}
+
+ComplexNum ComplexNum::operator*(int n){
+	this->a *= n;
+	this->b *= n;
+	return *this;
+}
